dee6b42f8ed7b69374e3183a7286cc9b5d0c5382
masterdd15 <masterdd15@gmail.com>
2023-02-25 18:39:02 -0500

    Finished Basic Button Logic
    The button now acts as a detection zone. It detects if any object stays inside it's trigger, or if any object leaves it's trigger. This makes it so an object has to be left inside of it in order to stay on, although this should be specified to be objects like the player or the crate. Will work on implementing this with James' logic next

--- LostEuclidean/Assets/DylansFolder/Scripts/Button/ButtonLogic.cs
+++ LostEuclidean/Assets/DylansFolder/Scripts/Button/ButtonLogic.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class ButtonLogic : MonoBehaviour
 {
+    [SerializeField]private ButtonState localState; 
+    private void Awake()
+    {
+        //This button's collider is attached to the parent object. Note this in case any changes are made
+        
+        localState = ButtonState.OFF; //Button will start off, and will wait until object are detected inside of it
+
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +24,23 @@ public class ButtonLogic : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        localState = ButtonState.ON;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        localState = ButtonState.OFF;
+    }
+
+    //I'm creating a state machine for the button
+    //Right now it functions as a bool, but we may want to add different weights or pressures later on
+    private enum ButtonState
+    {
+        ON,
+        OFF
     }
 }
