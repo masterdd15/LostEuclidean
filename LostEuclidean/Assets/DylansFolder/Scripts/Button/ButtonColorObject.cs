925e71e20cb50fe557e77720ef5d319749ea89b4
James Martini <jamesmartini1008@gmail.com>
2023-02-27 11:52:47 -0500

    color objects all work now
    color objects all basically working now

---
+++ LostEuclidean/Assets/DylansFolder/Scripts/Button/ButtonColorObject.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonColorObject : ColorObject
+{
+    protected void Start()
+    {
+        if (gameObject.layer == LayerMask.NameToLayer("Default"))
+        {
+            EnablePhysics();
+        }
+        else
+        {
+            DisableCollider();
+            DisablePhysics();
+        }
+    }
+
+    public override void OnLightEnter(LightColor lightColor)
+    {
+        base.OnLightEnter(lightColor);
+        if (!isLightActive)
+            return;
+        if (lightColor == baseColor)
+        {
+            EnablePhysics();
+        }
+        else
+        {
+            DisableCollider();
+            DisablePhysics();
+        }
+    }
+
+    public override void OnLightExit(LightColor lightColor)
+    {
+        base.OnLightExit(lightColor);
+        if (!isLightActive)
+            return;
+        DisableCollider();
+        DisablePhysics();
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        if (gameObject.layer == LayerMask.NameToLayer("Default"))
+        {
+            EnablePhysics();
+        }
+    }
+}
