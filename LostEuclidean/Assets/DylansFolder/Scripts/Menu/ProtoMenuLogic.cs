64f52e359d36ce43aaa996dd47d290ccace8c784
masterdd15 <masterdd15@gmail.com>
2023-03-01 12:55:00 -0500

    Created Level Selector
    The menu now features a level selector, and a back function. I will try and make sure that when the puzzle finished, the player is sent back to the menu

--- LostEuclidean/Assets/DylansFolder/Scripts/Menu/ProtoMenuLogic.cs
+++ LostEuclidean/Assets/DylansFolder/Scripts/Menu/ProtoMenuLogic.cs
@@ -5,14 +5,47 @@ using UnityEngine.SceneManagement;
 
 public class ProtoMenuLogic : MonoBehaviour
 {
-    
+    //This logic will handle all the different menu screens off our game
+    //We store each menu logic within GameObject
+
+    //0 = Menu Base
+    //1 = Menu Level Select
+    [SerializeField] List<GameObject> menuScreens;
+
+    //At the start of the menu, we should set every menu screen off, and turn on base
+    public void Awake()
+    {
+        //Goes through every item in a menu
+        for(int i = 0; i < menuScreens.Count; i++)
+        {
+            //If item is base menu, turn it on
+            if(i == 0)
+            {
+                menuScreens[i].SetActive(true);
+            }
+            else
+            {
+                menuScreens[i].SetActive(false);
+            }
+        }
+    }
+
+
+    //This button will show the level select screen, and hide the base menu screen
     public void HandleStartButton()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        menuScreens[0].SetActive(false);
+        menuScreens[1].SetActive(true);
     }
 
     public void HandleExitButton()
     {
         Application.Quit();
+    }
+
+    public void HandleMenu_Level_Back()
+    {
+        menuScreens[1].SetActive(false); //Hide Level Menu
+        menuScreens[0].SetActive(true); //Show Base Menu
     }
 }
