6c9ab45d1d1e3686993eb5a21b8cb8ffbf8017d9
Xegnis <xegnis@gmail.com>
2023-03-08 12:00:56 -0500

    Added Script Control for Glitch Visuals

--- LostEuclidean/Assets/Scripts/ColorObject.cs
+++ LostEuclidean/Assets/Scripts/ColorObject.cs
@@ -10,20 +10,29 @@ public class ColorObject : MonoBehaviour
     [SerializeField]
     protected bool interactable = false;
     [SerializeField]
-    protected int colorObjectID = 0;
+    protected bool canGlitch = false;
+
+    [Space(10), Header("Dependencies"), SerializeField]
+    protected Material[] glitchMaterials = new Material[3];
 
     protected Collider col;
     protected ColorRoom room;
     protected Rigidbody rigidBody;
+    protected MeshRenderer meshRenderer;
+    [SerializeField]
+    protected ParticleSystem glitchParticles; //component of direct child object
 
     protected bool isLightActive = true;
     protected bool canInteract = false;
+    protected bool isGlitching = false;
 
     protected virtual void Awake()
     {
         col = GetComponent<Collider>();
         room = GetComponentInParent<ColorRoom>(); //TODO: support nested parents
         rigidBody = GetComponent<Rigidbody>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        glitchParticles = GetComponentInChildren<ParticleSystem>();
     }
 
     protected virtual void OnEnable()
@@ -70,6 +79,42 @@ public class ColorObject : MonoBehaviour
         {
             rigidBody.isKinematic = true;
         }
+    }
+
+    protected void StartGlitching()
+    {
+        if (isGlitching || !canGlitch)
+            return;
+        isGlitching = true;
+        Material[] materials = meshRenderer.materials;
+        Material[] newMaterials = new Material[materials.Length + 3];
+        for (int i = 0; i < materials.Length; i++)
+        {
+            newMaterials[i] = materials[i];
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            newMaterials[materials.Length + i] = glitchMaterials[i];
+        }
+        meshRenderer.materials = newMaterials;
+        if (glitchParticles != null)
+            glitchParticles.Play();
+    }
+
+    protected void StopGlitching()
+    {
+        if (!isGlitching)
+            return;
+        isGlitching = false;
+        Material[] materials = meshRenderer.materials;
+        Material[] newMaterials = new Material[materials.Length - 3];
+        for (int i = 0; i < newMaterials.Length; i++)
+        {
+            newMaterials[i] = materials[i];
+        }
+        meshRenderer.materials = newMaterials;
+        if (glitchParticles != null)
+            glitchParticles.Stop();
     }
 
     //Put the object onto the right layer based on room color
