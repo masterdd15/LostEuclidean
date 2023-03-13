14b02600f9ac10e59de2c9a65fdf6b3ed354ae63
Xegnis <xegnis@gmail.com>
2023-03-13 19:33:01 -0400

    Added Dimension Lock Functionality

--- LostEuclidean/Assets/Scripts/ColorObject.cs
+++ LostEuclidean/Assets/Scripts/ColorObject.cs
@@ -11,6 +11,8 @@ public class ColorObject : MonoBehaviour
     protected bool interactable = false;
     [SerializeField]
     protected bool canGlitch = false;
+    [SerializeField]
+    protected bool canCauseGlitch = false;
 
     [Space(10), Header("Dependencies"), SerializeField]
     protected Material[] glitchMaterials = new Material[3];
@@ -22,9 +24,12 @@ public class ColorObject : MonoBehaviour
     [SerializeField]
     protected ParticleSystem glitchParticles; //component of direct child object
 
-    protected bool isLightActive = true;
+    public bool isRevealed = false; //true if visible
+    protected bool isLightActive = true; //true if on a LightColor layer
     protected bool canInteract = false;
     protected bool isGlitching = false;
+
+    protected List<ColorObject> glitchersTouching = new List<ColorObject>();
 
     protected virtual void Awake()
     {
@@ -40,8 +45,24 @@ public class ColorObject : MonoBehaviour
         UpdateColorLayer();
     }
 
-    public virtual void OnLightEnter(LightColor lightColor) { }
-    public virtual void OnLightExit(LightColor lightColor) { }
+    public virtual void OnLightEnter(LightColor lightColor)
+    {
+        if (!isLightActive)
+            return;
+        if (lightColor != baseColor)
+            return;
+        isRevealed = true;
+    }
+
+    public virtual void OnLightExit(LightColor lightColor)
+    {
+        if (!isLightActive)
+            return;
+        if (!isRevealed)
+            return;
+        isRevealed = false;
+    }
+
     public virtual void Interact() { }
 
     //Bunch of handy functions
@@ -86,6 +107,7 @@ public class ColorObject : MonoBehaviour
         if (isGlitching || !canGlitch)
             return;
         isGlitching = true;
+        canInteract = false;
         Material[] materials = meshRenderer.materials;
         Material[] newMaterials = new Material[materials.Length + 3];
         for (int i = 0; i < materials.Length; i++)
@@ -106,6 +128,7 @@ public class ColorObject : MonoBehaviour
         if (!isGlitching)
             return;
         isGlitching = false;
+        canInteract = true;
         Material[] materials = meshRenderer.materials;
         Material[] newMaterials = new Material[materials.Length - 3];
         for (int i = 0; i < newMaterials.Length; i++)
@@ -124,6 +147,7 @@ public class ColorObject : MonoBehaviour
         {
             gameObject.layer = LayerMask.NameToLayer("Default");
             isLightActive = false;
+            isRevealed = true;
             return;
         }
         
@@ -151,5 +175,58 @@ public class ColorObject : MonoBehaviour
     public bool CanInteract()
     {
         return canInteract;
+    }
+
+    protected void OnTriggerEnter(Collider other)
+    {
+        ColorObject co = other.GetComponent<ColorObject>();
+        if (co != null)
+        {
+            if (canGlitch && co.canCauseGlitch)
+            {
+                if (!glitchersTouching.Contains(co))
+                    glitchersTouching.Add(co);
+                //update itself as other color objects' glitcher
+                foreach (ColorObject glitcher in glitchersTouching)
+                {
+                    if (canCauseGlitch && glitcher.canGlitch && !glitcher.glitchersTouching.Contains(this))
+                        glitcher.glitchersTouching.Add(this);
+                }
+            }
+        }
+    }
+
+    protected void OnTriggerExit(Collider other)
+    {
+        ColorObject co = other.GetComponent<ColorObject>();
+        if (co != null)
+        {
+            if (glitchersTouching.Contains(co))
+            {
+                glitchersTouching.Remove(co);
+            }
+            UpdateGlitchState();
+        }
+    }
+
+    protected void UpdateGlitchState ()
+    {
+        if ((isRevealed || baseColor == room.roomColor) && glitchersTouching.Count > 0)
+        {
+            foreach (ColorObject glitcher in glitchersTouching)
+            {
+                if (glitcher.isRevealed == true)
+                {
+                    StartGlitching();
+                    return;
+                }
+            }
+        }
+        StopGlitching();
+    }
+
+    protected void LateUpdate()
+    {
+        UpdateGlitchState();
     }
 }
