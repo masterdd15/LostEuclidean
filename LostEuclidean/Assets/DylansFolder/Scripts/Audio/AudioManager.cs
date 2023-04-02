c9bc52b339992ea269f63d526edfe3c7f3f76f1e
masterdd15 <masterdd15@gmail.com>
2023-04-02 15:31:47 -0400

    Audio Manager Multiple color capabilities
    The Audio Manager now is capable of playing different dimension's audio! I will now work towards triggering these changes, and making sure it fades between clips

--- LostEuclidean/Assets/DylansFolder/Scripts/Audio/AudioManager.cs
+++ LostEuclidean/Assets/DylansFolder/Scripts/Audio/AudioManager.cs
@@ -7,6 +7,17 @@ public class AudioManager : MonoBehaviour
     //We are going to make sure there is only one audio manager between scenes
     public static AudioManager Instance;
 
+    //The Music Source is a bit special, so we need seperate sources for each dimensions music
+    [Header("Music Sources")]
+    [SerializeField] private AudioSource _MusicGreenSource;
+    [SerializeField] private AudioSource _MusicRedSource;
+    [SerializeField] private AudioSource _MusicBlueSource;
+    [SerializeField] private LightColor currentDimension;
+
+    //These will be composed of ur sound effect sources
+    [Header("FX Sources")]
+    [SerializeField] private AudioSource _FXSource;
+
     //We need to organize our sound clip variables, as to not confuse them for other references
     [Header("Background Audio")]
     [SerializeField] AudioClip greenDimensionMusic;
@@ -16,11 +27,6 @@ public class AudioManager : MonoBehaviour
     [Header("Button FX")]
     [SerializeField] AudioClip buttonActivate;
     [SerializeField] AudioClip buttonDeactivate;
-
-    //We should include a reference to our children Audio Sources
-    [Header("Audio Sources")]
-    [SerializeField] private AudioSource _BackgroundSource;
-    [SerializeField] private AudioSource _FXSource;
 
     private void Awake()
     {
@@ -40,16 +46,61 @@ public class AudioManager : MonoBehaviour
     //When we start a scene, this function will make sure that all of our sounds are set up properly
     private void Start()
     {
-        if(_BackgroundSource.clip == null)
-        {
-            SetBackground(greenDimensionMusic);
-        }
+        //We need to intialize all of our clips into our audio sources
+        SetupColorMusic();
+
+        //We need to know what color music we are using. We can find this from our Color Room object
+        LightColor currentColor = GameObject.Find("Color Room").GetComponent<ColorRoom>().roomColor;
+
+        //Now, we can handle the volume for what dimension should be playing
+        HandleColorMusic(currentColor);
+
+
     }
 
-    private void SetBackground(AudioClip music)
+    //This method is just putting a clip into an audio source.
+    private void SetupColorMusic()
     {
-        _BackgroundSource.clip = music;
-        _BackgroundSource.Play();
+        //We set all music to their respective sources, set their volumes to 0, and play them
+        //(Is there an easier/cleaner way to do this? Should look into it.)
+        //Need a way to be able to tell what color dimension each source is linked to.
+        _MusicGreenSource.clip = greenDimensionMusic;
+        _MusicGreenSource.volume = 0;
+        _MusicGreenSource.Play();
+
+        _MusicRedSource.clip = redDimensionMusic;
+        _MusicRedSource.volume = 0;
+        _MusicRedSource.Play();
+
+        _MusicBlueSource.clip = blueDimensionMusic;
+        _MusicBlueSource.volume = 0;
+        _MusicBlueSource.Play();
+    }
+
+    //In this function, we choose what which audio source will be heard depending on the rooms color
+    private void HandleColorMusic(LightColor roomColor)
+    {
+        switch(roomColor)
+        {
+            case LightColor.Green:
+                _MusicGreenSource.volume = 1;
+                _MusicRedSource.volume = 0;
+                _MusicBlueSource.volume = 0;
+                break;
+            case LightColor.Red:
+                _MusicRedSource.volume = 1;
+                _MusicGreenSource.volume = 0;
+                _MusicBlueSource.volume = 0;
+                break;
+            case LightColor.Blue:
+                _MusicBlueSource.volume = 1;
+                _MusicGreenSource.volume = 0;
+                _MusicRedSource.volume = 0;
+                break;
+            default:
+                Debug.Log("ERROR: MUSIC COLOR NOT FOUND");
+                break;
+        }
     }
 
     public void PlaySound(AudioClip clip)
