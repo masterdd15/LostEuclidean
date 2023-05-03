0d1e6b3f21807257b3b7649ee996c39a7b97c0c6
masterdd15 <masterdd15@gmail.com>
2023-05-03 11:59:47 -0400

    Dad can now interact
    You can now interact with your dad! Imagine!!!!

--- LostEuclidean/Assets/DylansFolder/Scripts/Finale/DadInteract.cs
+++ LostEuclidean/Assets/DylansFolder/Scripts/Finale/DadInteract.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class DadInteract : Interactable
 {
+    public override void Start()
+    {
+        base.Start();
+    }
 
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public override void Interact()
+    {
+        Debug.Log("Has interacted!");
+    }
 }
