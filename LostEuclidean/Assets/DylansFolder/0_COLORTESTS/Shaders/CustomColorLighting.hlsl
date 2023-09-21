7954d09436df1fb8089a1aa21658f9b333b396c9
masterdd15 <masterdd15@gmail.com>
2023-09-20 23:23:16 -0400

    Finished tutorial
    I know have a full custom light shader? Will try to tinker with this tomorrow to see if we can display any artistic research in

--- LostEuclidean/Assets/DylansFolder/0_COLORTESTS/Shaders/CustomColorLighting.hlsl
+++ LostEuclidean/Assets/DylansFolder/0_COLORTESTS/Shaders/CustomColorLighting.hlsl
@@ -1,6 +1,16 @@
 #ifndef CUSTOM_LIGHTING_INCLUDED
 #define CUSTOM_LIGHTING_INCLUDED
 
+// This is a neat trick to work around a bug in the shader graph when
+// enabling shadow keywords. Created by @cyanilux
+// https://github.com/Cyanilux/URP_ShaderGraphCustomLighting
+#ifndef SHADERGRAPH_PREVIEW
+    #include "Packages/com.unity.render-pipelines.universal/Editor/ShaderGraph/Includes/ShaderPass.hlsl"
+    #if (SHADERPASS != SHADERPASS_FORWARD)
+        #undef REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR
+    #endif
+#endif
+
 struct CustomLightingData 
 {
 	//Position and orientation
@@ -12,6 +22,12 @@ struct CustomLightingData
 	//Surface attributes
 	float3 albedo;
 	float smoothness;
+	float ambientOcclusion;
+
+	//Baked Lighting
+	float3 bakedGI;
+	float4 shadowMask;
+	float fogFactor;
 };
  
 //Translate a [0,1] smoothness value to an exponent
@@ -21,6 +37,24 @@ float GetSmoothnessPower(float rawSmoothness)
 } 
 
 #ifndef SHADERGRAPH_PREVIEW
+float3 CustomGlobalIllumination(CustomLightingData d)
+{
+	float3 indirectDiffuse = d.albedo * d.bakedGI * d.ambientOcclusion;
+
+	float3 reflectVector = reflect(-d.viewDirectionWS, d.normalWS);
+	//This is a rim light term, making reflections stronger along
+	//the edge of view 
+	float3 fresnel = Pow4(1 - saturate(dot(d.viewDirectionWS, d.normalWS)));
+	//This function samples the baked reflection cubemap
+	//It is located in URP/ShaderLibrary/Lighting.hlsl
+	float3 indirectSpecular = GlossyEnvironmentReflection(reflectVector,
+		RoughnessToPerceptualRoughness(1 - d.smoothness), 
+		d.ambientOcclusion) * fresnel;
+
+	return indirectDiffuse;
+}
+
+
 float3 CustomLightHandling(CustomLightingData d, Light light)
 {
 	float3 radiance = light.color * (light.distanceAttenuation * light.shadowAttenuation);
@@ -45,28 +79,33 @@ float3 CalculateCustomLighting(CustomLightingData d)
 	return d.albedo * intensity;
 #else
 	//Get the main light. Located in URP/ShaderLibrary/Lighting.hlsl
-	Light mainlight = GetMainLight(d.shadowCoord, d.positionWS, 1);
-
-	float3 color = 0;
+	Light mainLight = GetMainLight(d.shadowCoord, d.positionWS, d.shadowMask);
+	// In mixed subtractive baked lights, the main light must be subtracted
+	// from the bakedGI value. This function in URP/ShaderLibrary/Lighting.hlsl takes care of that
+	MixRealtimeAndBakedGI(mainLight, d.normalWS, d.bakedGI);
+	float3 color = CustomGlobalIllumination(d);
 	//Shade the main light
-	color += CustomLightHandling(d, mainlight);
+	color += CustomLightHandling(d, mainLight);
 
 	#ifdef _ADDITIONAL_LIGHTS
 		//Shade additional cone and point lights. Functions in URP/ShaderLibrary/Lighting.hlsl
 		uint numAdditionalLights = GetAdditionalLightsCount();
 		for(uint lightI = 0; lightI < numAdditionalLights; lightI++)
 		{
-			Light light = GetAdditionalLight(lightI, d.positionWS, 1);
+			Light light = GetAdditionalLight(lightI, d.positionWS, d.shadowMask);
 			color += CustomLightHandling(d, light);
 		}
 	#endif
+
+	color = MixFog(color, d.fogFactor);
 
 	return color;
 #endif
 }
 
 void CalculateCustomLighting_float(float3 Position, float3 Normal, float3 ViewDirection, 
-	float3 Albedo, float Smoothness,
+	float3 Albedo, float Smoothness, float AmbientOcclusion,
+	float2 LightmapUV,
 	out float3 Color)
 {
 	CustomLightingData d;
@@ -75,10 +114,14 @@ void CalculateCustomLighting_float(float3 Position, float3 Normal, float3 ViewDi
 	d.viewDirectionWS = ViewDirection;
 	d.albedo = Albedo;
 	d.smoothness = Smoothness;
+	d.ambientOcclusion = AmbientOcclusion;
 	
 #ifdef SHADERGRAPH_PREVIEW
 	//In preview, there's no shadows or bakedGI
 	d.shadowCoord = 0;
+	d.bakedGI = 0;
+	d.shadowMask = 0;
+	d.fogFactor = 0;
 #else
 	//Calculate the main light shadow coord
 	//There are two types depending on if cascades are enabled
@@ -88,6 +131,27 @@ void CalculateCustomLighting_float(float3 Position, float3 Normal, float3 ViewDi
 	#else
 		d.shadowCoord = TransformWorldToShadowCoord(Position);
 	#endif
+	
+	// The following URP functions and macros are all located in
+    // URP/ShaderLibrary/Lighting.hlsl
+    // Technically, OUTPUT_LIGHTMAP_UV, OUTPUT_SH, VertexLighting and ComputeFogFactor
+    // should be called in the vertex function of the shader. However, as of
+    // 2021.1, we do not have access to custom interpolators in the shader graph.
+
+    // The lightmap UV is usually in TEXCOORD1
+    // If lightmaps are disabled, OUTPUT_LIGHTMAP_UV does nothing
+    float2 lightmapUV;
+    OUTPUT_LIGHTMAP_UV(LightmapUV, unity_LightmapST, lightmapUV);
+    // Samples spherical harmonics, which encode light probe data
+    float3 vertexSH;
+    OUTPUT_SH(Normal, vertexSH);
+    // This function calculates the final baked lighting from light maps or probes
+    d.bakedGI = SAMPLE_GI(lightmapUV, vertexSH, Normal);
+	//This function calculates the shadow mask if baked shadows are enabled
+	d.shadowMask = SAMPLE_SHADOWMASK(lightmapUV);
+	//This returns 0 if fog is turned off
+	//It is not the same as the fog node in the shader graph
+	d.fogFactor = ComputeFOgFactor(positionCS.z);
 
 #endif
 	
